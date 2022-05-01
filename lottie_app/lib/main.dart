1c7352ec1092063ddb99fdb45fa92151fde78225
SherazJamil <sherazjamil463@gmail.com>
2022-05-01 12:36:57 +0500

    Lottie Animation
    Lottie animation in Flutter

---
+++ lottie_app/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+  with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(
+          'Lottie Animation',
+        ),
+      ),
+      body: Center(
+        child: Lottie.network(
+          'https://assets1.lottiefiles.com/private_files/lf30_0iauxl5e.json',
+          repeat: true,
+          controller: _controller,
+          onLoaded: (composition){
+            _controller.duration = composition.duration;
+            _controller.repeat();
+          }
+        ),
+      ),
+    );
+  }
+}
